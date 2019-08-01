38ebfe5bbb201bc95f90bd3cba2dfffd93766adb
Jesus Javier Puente <chuchip@gmail.com>
2019-08-01 07:08:49 +0200

    Mejorando documentación

--- src/main/java/com/profesorp/restTemplate/server/dto/Customer.java
+++ src/main/java/com/profesorp/restTemplate/server/dto/Customer.java
@@ -1,12 +1,9 @@
 package com.profesorp.restTemplate.server.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class Customer {
-
 	private String name;
 	private String address;
 }
