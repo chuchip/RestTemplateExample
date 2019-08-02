7b516c57d37f30061759ece182b7f131dcbd19c4
Jesus Javier Puente <chuchip@gmail.com>
2019-08-02 23:16:46 +0200

    Avanzando...

--- src/main/java/com/profesorp/restTemplate/RestTemplateExampleApplication.java
+++ src/main/java/com/profesorp/restTemplate/RestTemplateExampleApplication.java
@@ -1,19 +1,12 @@
 package com.profesorp.restTemplate;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -26,10 +19,10 @@ public class RestTemplateExampleApplication {
 	@Bean
 	public RestTemplate createRestTemplate(CustomResponseErrorHandler errorHandler) {
 		RestTemplate restTemplate = new RestTemplate();				
-		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();		
-		interceptors.add(new LoggingRequestInterceptor(errorHandler));
+		restTemplate.setErrorHandler(errorHandler);
+//		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();		
+//		interceptors.add(new LoggingRequestInterceptor(errorHandler));
 //		restTemplate.setInterceptors(interceptors);
-//		restTemplate.setErrorHandler(errorHandler);
 		return restTemplate;
 	}
 
--- src/main/java/com/profesorp/restTemplate/impl/RestClient.java
+++ src/main/java/com/profesorp/restTemplate/impl/RestClient.java
@@ -1,5 +1,6 @@
 package com.profesorp.restTemplate.impl;
 
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -14,80 +15,77 @@ import com.profesorp.restTemplate.CustomResponseErrorHandler;
 
 /**
  * Llamada a RestTemplate sin personalizar.
+ *
  * @author usuario
  *
  */
 @Component
 public class RestClient {
+    @Autowired
+    RestTemplate restTemplate;
 
-	@Autowired
-	RestTemplate restTemplate;
+    @Autowired
+    CustomResponseErrorHandler customError;
 
-	@Autowired
-	CustomResponseErrorHandler customError;
+    @Autowired
+    Environment enviroment;
 
-	@Autowired
-	Environment enviroment;
+    String url = "http://localhost:";
 
-	String url = "http://localhost:";
+    @PostConstruct
+    public void iniciar() {
+        String puerto = enviroment.getProperty("server.port");
+        if (puerto == null) {
+            puerto = "8080";
+        }
+        url = url + puerto;
+    }
 
-	public RestClient() {
-		String puerto = enviroment.getProperty("server.port");
-		if (puerto == null)
-			puerto = "8080";
-		url = url + puerto;
-	}
+    public String peticionGetPersonalizada(String idCliente) {
+       
+        String localUrl = url;
+        if (idCliente != null) {
+            localUrl += "?nameCustomer=" + idCliente;
+        }
+        if ("DOWN".equals(idCliente)) {
+            localUrl = "http://localhost:1111";
+        }
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForEntity(localUrl, String.class);
+        } catch (RestClientException k) {
+            return  "Custom RestTemplate. The server didn't respond: " + k.getMessage();
+        }
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        String mensaje = "Http Status: " + httpStatus + " -> ";
+        if (httpStatus.is2xxSuccessful()) {
+            mensaje += "Body: " + responseEntity.getBody();
+        } else {
+            mensaje += " Message error: "+ customError.getBody();
+        }
+        return mensaje;
+    }
 
-	public String peticionGetPersonalizada(String idCliente) {
-		String localUrl = url;
-		if (idCliente != null)
-			localUrl += "?nameCustomer=" + idCliente;
-		ResponseEntity<String> responseEntity = null;
-		try {
-			responseEntity = restTemplate.getForEntity(localUrl, String.class);
+    public String peticionGet(String idCliente) {
+        String localUrl = url;
+        if (idCliente != null) {
+            localUrl += "?nameCustomer=" + idCliente;
+        }
+        if ("DOWN".equals(idCliente)) {
+            localUrl = "http://localhost:1111";
+        }
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = new RestTemplate().getForEntity(localUrl, String.class);
+        } catch (HttpClientErrorException k1) {            
+            return "Http code is not 2XX. The server responded: " + k1.getStatusCode() + " Cause: "
+                    + k1.getResponseBodyAsString();
+        } catch (RestClientException k) {
+            return "The server didn't respond: " + k.getMessage();
+        }
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        String mensaje = "Http Status: " + httpStatus + " -> " + responseEntity.getBody();
+        return mensaje;
+    }
 
-		} catch (RestClientException k) {
-			String msg = "Error tipo restClientException k1" + k.getMessage();
-			return msg;
-		}
-		HttpStatus httpStatus = responseEntity.getStatusCode();
-		String mensaje = "Http Status: " + httpStatus + " -> ";
-		if (httpStatus.is2xxSuccessful())
-			mensaje += "Body: " + responseEntity.getBody();
-		else
-			mensaje += customError.getBody();
-		return mensaje;
-	}
-
-	public String peticionGet(String idCliente) {
-		String localUrl = url;
-		if (idCliente != null)
-			localUrl += "?nameCustomer=" + idCliente;
-		ResponseEntity<String> responseEntity = null;
-		try {
-			responseEntity = new RestTemplate().getForEntity(localUrl, String.class);
-
-		} catch (HttpClientErrorException k1) {
-			String msg = "Error al realizar petición HTTP. Codigo retornado: " + k1.getStatusCode() + " Causa: "
-					+ k1.getResponseBodyAsString();
-			return msg;
-		} catch (RestClientException k) {
-			String msg = "Error tipo restClientException k1" + k.getMessage();
-			return msg;
-		}
-		HttpStatus httpStatus = responseEntity.getStatusCode();
-		String mensaje = "Http Status: " + httpStatus + " -> "+	responseEntity.getBody();	
-		 
-		return mensaje;
-	}
-
-	public String peticionServerDown() {
-		String localUrl = "http://localhost:1111";
-		try {
-			new RestTemplate().getForEntity(localUrl, String.class);
-		} catch (RestClientException k) {
-			return "Error al realizar petición HTTP ->" + k.getMessage();
-		}
-		return null;
-	}
 }
